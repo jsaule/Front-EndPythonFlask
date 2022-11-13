5cc1d170b6e26e5c947ed02ba9d3c40abf119082
Julija <julija.sauleniene@gmail.com>
2022-11-13 22:11:23 +0200

    Add sql_alchemy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 # creates Flask instance
 app = Flask(__name__)
+
+# set configs
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+app.config['SECRET_KEY'] = 'SSDSDDSDSDSFSLGJNAOOAJGOAJNWARGAWRGAWRG'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# create database
+db = SQLAlchemy()
 
 # --------------------------------external routes----------------------------------------------------
 # about us page

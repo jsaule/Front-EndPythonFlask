65dcf90d8b99e7b9554fa00c60e3e0553c6258fc
Julija <julija.sauleniene@gmail.com>
2022-11-13 21:06:34 +0200

    Add error handlers

--- app/main.py
+++ app/main.py
@@ -4,8 +4,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # --------------------------------external routes----------------------------------------------------
+# about us page
 @app.route('/aboutus')
 def aboutus():
     return render_template("aboutus.html")
 
-# --------------------------------requiring authentication routes----------------------------------------------------
+# invalid URL
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+# internal server error
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template("500.html"), 500
+
+# --------------------------------requiring authentication routes--------------------------------------
